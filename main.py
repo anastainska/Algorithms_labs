0602ac38a591dc8191c08e422fb5e48ef3f587a1
Anastasiia Kainska <anastasia.kajinska@gmail.com>
2022-06-10 11:08:28 +0300

    adding all files for lab 6

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
-from graph import Graph
+def find_ways_count(matrix, w, h):
+    ways_arr = [[1 for _ in range(w)] for _ in range(h)]
+
+    incrementer_for_letter = dict()
+    letter_was_in_column = dict()
+
+    for i in range(w):
+        for j in range(h):
+            letter = matrix[j][i]
+            if i+1 < w:
+                if matrix[j][i] == matrix[j][i+1]:
+                    ways_arr[j][i+1] -= 1
+
+            if letter in incrementer_for_letter:
+                ways_arr[j][i] += incrementer_for_letter[letter]
+
+            if letter in letter_was_in_column:
+                letter_was_in_column[letter] += ways_arr[j][i]
+            else:
+                letter_was_in_column[letter] = ways_arr[j][i]
+
+        for (letter, count) in letter_was_in_column.items():
+            if letter in incrementer_for_letter:
+                incrementer_for_letter[letter] += letter_was_in_column[letter]
+            else:
+                incrementer_for_letter[letter] = letter_was_in_column[letter]
+        letter_was_in_column = dict()
+
+    return ways_arr
 
 
 if __name__ == '__main__':
-    graph = {
-        1: [[2, 13], [3, 18], [4, 17], [5, 14], [6, 22]],
-        2: [[3, 26], [5, 19]],
-        3: [[4, 30]],
-        4: [[6, 22]]
-    }
-    g = Graph(6, graph)
-    print(g.prims_algorithm())
+
+    w, h = map(int, input().split(" "))
+
+    matrix = [list(input())[:w] for i in range(h)]
+    ways_count = find_ways_count(matrix, w, h)
+
+    print(ways_count[0][w-1] + ways_count[h-1][w-1])
